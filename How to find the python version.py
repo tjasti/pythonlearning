949259a279d1f5f0e49cc4830e23db340d876dec
tjasti <36494623+tjasti@users.noreply.github.com>
2019-12-10 14:16:03 -0500

    finding out the pythonversion

---
+++ How to find the python version.py
@@ -0,0 +1,7 @@
+"""Python –version"""
+"""For suppose, if you have more than 2 versions on your machine,
+ then"""
+"""Python3 –version"""
+"""(Which gives the version that starts with 3)"""
+"""Python2 –version
+(Which gives the version that starts with 2)"""
