310c666c3ba047eeda8c0bd15a2ebbc03d87c894
tjasti <36494623+tjasti@users.noreply.github.com>
2019-12-10 15:03:11 -0500

    string methods

---
+++ string methods.py
@@ -0,0 +1,21 @@
+#String methods
+string="Hello World"
+#String upper
+#String lower
+#String replace
+#find index
+#contains
+#how to do slicing with the strings
+"""Below method will convert the string to upper case"""
+print(string.upper())
+#String lower
+print(string.lower())
+"""The above method will convert all the characters to lowercase letters"""
+"""Find method will find the index of the given string,if the given string is not found , it will return -1"""
+print(string.find("Wo"))
+print(string.count('o'))
+"""count method will count the occurences of the characters"""
+print(len(string))
+print(string.replace("o", "_"))
+"""replace method will replace the old characters with new ones"""
+print(string)
