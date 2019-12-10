6f546391fde28ce84f126a8f5f7e15e1f2aa23c5
tjasti <36494623+tjasti@users.noreply.github.com>
2019-12-10 14:11:18 -0500

    python variables program

---
+++ python variables.py
@@ -0,0 +1,18 @@
+"""Like other programing languages we don't need
+ to specify the type of the variable"""
+a=5
+print(a);
+b=10
+print(a+b)
+"""for suppose, if we have strings, how do we merge that"""
+string1="Hi"
+string2="Welcome!"
+print(string1+" "+string2)
+"""For suppose, if we have two different kinds of variables then"""
+#print(a+string1)
+"""Then the error will be as below"""
+"""unsupported operand type(s) for +: 'int' and 'str'"""
+print(a,string1)
+"""with comma we can use different kinds of variables"""
+"""in order to priint the multiple lines of text
+we use three quotation marks"""
