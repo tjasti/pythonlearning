5928c62fa6ca685561f977be527984fcdb117760
tjasti <36494623+tjasti@users.noreply.github.com>
2019-12-10 11:12:41 -0500

    lists program

---
+++ lists.py
@@ -0,0 +1,12 @@
+subjects=['maths','science','social']
+print(subjects)
+subjects.append('english')
+print("after appending")
+print(subjects)
+#clear function, it will clear the items from the list but it will keep the structure
+#subjects.clear()
+print(subjects)
+#Adding items back to the list
+#copy method
+subjects1=subjects.copy()
+print(subjects1)
