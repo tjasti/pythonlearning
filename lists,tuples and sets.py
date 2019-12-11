47fc882e685cf6baa3f852e0a5cf7edce410c452
tjasti <36494623+tjasti@users.noreply.github.com>
2019-12-10 21:58:54 -0500

    lists,tuples and sets methods of lists differences methods of sets

---
+++ lists,tuples and sets.py
@@ -0,0 +1,20 @@
+#In this, we will be discussing about lists,tuples and sets
+"""difference between Lists and Tuples"""
+"""We can make modifications to the lists,where as tuples cannot be modified"""
+"""sets cannot allow the duplicates of data"""
+subjects=['Maths','science','social']
+"""Mthods of a lis"""
+#append
+#remove
+#pop method
+#Index
+subjects.append("English")
+print(subjects)
+#append method adds the item to the end
+subjects.insert(4,"Science")
+#insert method inserts the item at the particular location
+print(subjects)
+#pop method removes the last item from the list
+subjects.pop()
+print(subjects)
+a=[7,2,3,5,8]
