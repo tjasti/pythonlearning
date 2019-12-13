25ba1074048a39e379446013c21faa96b318546a
tjasti <36494623+tjasti@users.noreply.github.com>
2019-12-13 11:51:09 -0500

    Programs with for loop,while,,ultiplication tables, break,continue

---
+++ loopsanditerations.py
@@ -0,0 +1,43 @@
+#scenario 1 from loopsanditerationstasks
+a=[1,5,6,7,4]
+for num in a:
+    print(num)
+#How to print the nu,bers in a list in reverse way using loops
+length=len(a)
+print("printing the numbers in a reverse order using for loop")
+for i in range(length-1,-1,-1):
+    print(a[i])
+print("enter a number to search in the list")
+userinput=int(input())
+print("  ")
+for j in a:
+    if(j==userinput):
+        break;
+        print("Found")
+    print(j)
+print("Break scenario is done")
+#Continue
+#scenario3
+for k in a:
+    if(k==userinput):
+        print(k,"Found")
+        continue
+    print(k)
+print("Continue scenario is done")
+#Scenario5
+#print numbers from 1 to 20
+for m in range(1,10,2):
+    print(m)
+#scenario 6  print a multiplication table using for loop
+print("Enter a number for multiplication table")
+table=int(input())
+for x in range(1,11):
+    print(table,"*",x,"=",(table*x))
+print("***********")
+#scenario 7 print("Enter a multiplication table using while loop")
+print("Using while loop")
+p1=1
+while (p1<=10):
+    print(table,"*",p1,"=",(table*p1))
+    p1=p1+1;
+print("***********")
---
+++ loopsanditerationstasks.py
@@ -0,0 +1,13 @@
+"""Scenario1:Take an array and print the values of that using loop"""
+"""Scenario 2:Take an input from the user and search for that number in the array using for loops(if the number id found , print found statement and then break the loop)"""
+"""Scenario 3:Use the above scenario and if the number is found keep continuing the loop"""
+"""Scenario 4:write a program to print the below output
+1 a
+2 bb
+3 c
+.....
+26 z"""
+"""Scenario#5:run the loop for certain number of loops using range"""
+"""Scenario#6:print the multiplication program by using for and while loop"""
+"""Scenario#7:write a program using infinite loop and stop the output by using keyboard shortcuts"""
+"""Scneario#8:Write a program to print the fibonacci series"""
