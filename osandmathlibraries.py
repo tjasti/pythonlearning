524735b4f3da3a57b9a0c15cd0c7d9106671344b
tjasti <36494623+tjasti@users.noreply.github.com>
2019-12-25 14:06:09 -0500

    import os,random and math

---
+++ osandmathlibraries.py
@@ -0,0 +1,17 @@
+import os
+import math
+import random
+print(math.pi)
+print(math.sqrt(25))
+print(math.sin(90))
+print(math.cos(0))
+print(math.log(1))
+print(math.ceil(3.4))
+#print(help(os))
+print(os.getcwd())
+print(os.chdir("C:\\Users\\jastit\\Desktop\\sample"))
+print(os.getcwd)
+print(os.chdir("C:\\Users\\jastit\\Desktop\\github"))
+a=list()
+a=[1,5,6,7,9,10]
+print(random.choice(a))
